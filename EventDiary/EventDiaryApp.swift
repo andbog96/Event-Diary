aebc1712e684fe1877660fe1f11d23da95f5d5e7
Andrey Bogdanov <andr.bogdanov@vk.team>
2024-11-06 13:02:29 +0300

    Initial Commit

---
+++ EventDiary/EventDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventDiaryApp.swift
+//  EventDiary
+//
+//  Created by Bogdanov Andrey on 06.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct EventDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
